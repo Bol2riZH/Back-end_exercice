d9a0b71229c071982039ef386928e9e22ecb09f0
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-08-04 07:13:55 +0200

    put get and getOne

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
 
 const app = express();
 
+mongoose
+  .connect(
+    'mongodb+srv://matthieu:atlKenshin@cluster0.zpmdqhm.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // autorise req.body below
 app.use(express.json());
+// app.use(bodyParser.json());
 
+// set header to authorize comm
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -18,33 +34,38 @@ app.use((req, res, next) => {
   next();
 });
 
+// post request
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json('Objet créé !');
+  delete req.body._id;
+  const thing = new Thing({
+    // title: req.body.title,
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: 'Object save !' }))
+    .catch(error => res.status(400).json({ error }));
 });
 
+// put
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Object modify!' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// get one obj
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+// get request
 app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
